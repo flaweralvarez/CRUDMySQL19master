f8b04bd0d57e6a8e124775af88643e1706fbc2dd
flaweralvarez <56841780+flaweralvarez@users.noreply.github.com>
2019-11-01 20:10:47 -0600

    subiendo mi proyecto

---
+++ app/src/main/java/com/example/crudmysql19_master/Dto.java
@@ -0,0 +1,47 @@
+package com.example.crudmysql19_master;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dto extends AppCompatActivity {
+
+    int codigo;
+    String descripcion;
+    double precio;
+
+
+    public Dto() {
+    }
+
+
+    public Dto(int codigo, String descripcion, double precio) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/crudmysql19_master/Dto_Login.java
@@ -0,0 +1,6 @@
+package com.example.crudmysql19_master;
+
+public class Dto_Login {
+
+
+}
